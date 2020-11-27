f68d3a1e3bfdc8af2885d56c1c85b149b08f3767
me-anmol <anmolbadi@gmail.com>
2020-11-27 21:32:18 +0530

    url

--- frequency/urls.py
+++ frequency/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path 
 from . import views
 urlpatterns = [
-    path('frequency/', views.index, name= 'index'),
+    path('', views.index, name= 'index'),
     path('result/',views.result, name = 'result')
 ]
